8fdf5e56296d33b0718990ae099c185716237ade
Jason Heppler <jason.heppler@gmail.com>
2022-10-18 14:09:05 -0500

    Special methods for model content.

--- ctapp/threads/models.py
+++ ctapp/threads/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class CooperItems(models.Model):
+class CooperItem(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     date = models.DateTimeField()
@@ -10,8 +10,10 @@ class CooperItems(models.Model):
     item_medium = models.CharField(max_length=255)
     item_image_url = models.CharField(max_length=255)
     item_location = models.CharField(max_length=255)
+    def __str__(self):
+        return self.title
 
-class VAItems(models.Model):
+class VAItem(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     date = models.DateTimeField()
@@ -20,3 +22,5 @@ class VAItems(models.Model):
     item_medium = models.CharField(max_length=255)
     item_image_url = models.CharField(max_length=255)
     item_location = models.CharField(max_length=255)
+    def __str__(self):
+        return self.title
